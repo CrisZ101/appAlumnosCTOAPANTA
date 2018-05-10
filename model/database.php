83ba713c5205651656feb3711023f31a9004a2c4
catoapanta <megacriszero@gmail.com>
2018-05-10 08:50:00 -0500

    Correccion Push
    Cadena

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('serveralumnado.mysql.database.azure.com', 'server@serveralumnado', 'crisz101.');
+        $pdo = new PDO('mysql:host=serveralumnado.mysql.database.azure.com;dbname=alumnado;charset=utf8', 'server@serveralumnado', 'crisz101.');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
